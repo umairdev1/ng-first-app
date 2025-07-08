9deac408f063d450cb3456a0b735c6359c7b9484
umairdev1 <umairdev4@gmail.com>
2025-07-08 23:41:19 +0500

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Signin } from './auth/signin/signin';
+import { Signup } from './auth/signup/signup';
+import { Project } from './project/project';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'signin', component: Signin },
+  { path: 'signup', component: Signup },
+  { path: 'projects', component: Project },
+];
